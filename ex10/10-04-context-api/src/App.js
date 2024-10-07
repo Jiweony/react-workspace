e64dfc4ba8444cc7e866ec5f48d58f1f5525d992
Jiweony <wjdwldnjs0419@naver.com>
2024-10-07 21:55:11 +0900

    react files uploaded

---
+++ ex10/10-04-context-api/src/App.js
@@ -0,0 +1,105 @@
+import React, { createContext, useCallback, useMemo, useReducer, useRef, useState } from "react";
+import UserList from "./UserList";
+import CreateUser from "./CreateUser";
+import useInputs from "./hooks/useInputs";
+
+function countActiveUsers(users){
+  console.log("활성 사용자 수를 세는중...");
+  return users.filter(user => user.active).length;
+}
+
+const initialState = {
+
+  inputs: {
+    username: '',
+    email: ''
+  },
+
+  users: [
+    {
+      id: 1,
+      username: '오일남',
+      email: 'o1@example.com',
+      active: false
+    },
+    {
+      id: 2,
+      username: '오이남',
+      email: 'o2@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: '오삼남',
+      email: 'o3@example.com',
+      active: false
+    },
+  ]
+}
+
+export const UserDispatch = createContext(null); //전역
+
+function reducer(state, action) {
+  const {name, value} = action.payload
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.payload.name]: action.payload.value
+        }
+      }
+    case "CREATE_USER":
+      const newArr = [...state.users, action.payload] 
+      return {inputs: initialState.inputs, users: newArr};
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) => user.id === action.payload ? {...user, active: !user.active} : user)
+      };
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.payload.id)
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [{username, email}, onChange, reset] = useInputs({
+    username: '',
+    email: ''
+  })
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const {users} = state;
+  const nextId = useRef(4);
+
+  const onCreate = useCallback(()=> {
+    dispatch({
+      type: "CREAE_USER",
+      payload: {
+      id: nextId.current,
+      username,
+      email
+      }
+    })
+    nextId.current += 1;
+    reset();
+  }, [username, email])
+
+  
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
+  return (
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} />
+      <div>활성사용자 수 : {count}</div>
+    </UserDispatch.Provider>
+  );
+}
+
+export default App;
---
+++ ex10/10-04-context-api/src/hooks/useInputs.js
@@ -0,0 +1,16 @@
+import { useCallback, useState } from "react";
+
+export default function useInputs(initialForm) {
+  const [form, setForm] = useState(initialForm);
+
+  const onChange = useCallback(e => {
+    const {name, value} = e.target;
+    setForm(prev => {
+      return {...prev, [name]: value};
+    })
+  }, [])
+    
+    const reset = useCallback(() => setForm(initialForm), [initialForm])
+
+  return [form, onchange, reset];
+}
